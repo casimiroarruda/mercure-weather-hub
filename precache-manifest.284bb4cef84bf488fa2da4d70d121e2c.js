127319f0448a547a04b1c601cdf4c80b99bfbca8
Anderson Casimiro <duodraco@gmail.com>
2019-09-25 00:00:46 -0300

    deploy

---
+++ precache-manifest.284bb4cef84bf488fa2da4d70d121e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "dbd5acec9b0f5082cac2",
+    "url": "/js/chunk-vendors.ec66f979.js"
+  },
+  {
+    "revision": "f17385e6b81cdc7a411a",
+    "url": "/js/app.116a69aa.js"
+  },
+  {
+    "revision": "ed5482b9b4b6239412cfdd74ddbc6fb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929f0d1954661d53b2a7220072f691a9",
+    "url": "/img/sara-the-freak-A4UojtraSrw-unsplash.929f0d19.jpg"
+  },
+  {
+    "revision": "dbd5acec9b0f5082cac2",
+    "url": "/css/chunk-vendors.fe0970cb.css"
+  }
+];
