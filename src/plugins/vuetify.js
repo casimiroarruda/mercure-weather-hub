3a4ea9b6d47d3dcd451a1a1986eaaa129296b645
Anderson Casimiro <duodraco@gmail.com>
2019-09-26 00:54:49 -0300

    visual fixes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,19 @@ import Vuetify from 'vuetify/lib'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#336699',
+        secondary: '#2a9f7d',
+        accent: '#3f51b5',
+        error: '#f44336',
+        warning: '#ff9800',
+        info: '#00bcd4',
+        success: '#8bc34a'
+      }
+    }
+  }
   // icons: {
   //   iconfont: 'mdi'
   // }
